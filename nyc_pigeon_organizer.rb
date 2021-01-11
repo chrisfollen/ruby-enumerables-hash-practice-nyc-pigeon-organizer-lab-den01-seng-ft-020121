ce9ef23205ef48996219b5cf2d4349a824382bda
Chris Follen <christopher.follen@gmail.com>
2021-01-11 18:56:05 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  hash_out = {}
+  data.each do |key, value|
+    value.each do |inside_key, names|
+      names.each do |current_name|
+        if !hash_out[current_name]
+          hash_out[current_name] = {}
+        end
+        if !hash_out[current_name][key]
+          hash_out[current_name][key] =[]
+        end
+        hash_out[current_name][key].push(inside_key.to_s)
+        
+      end
+    end
+  end
+  hash_out
+      
 end
+
